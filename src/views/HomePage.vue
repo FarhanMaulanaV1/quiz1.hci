ee2ff2cd9965324e357c81405daa95106c94e362
adamlazu <MOCHAMADADAMLAZUARDIAGUNG_1313621018@mhs.unj.ac.id>
2022-11-01 20:31:41 +0700

    first commit

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,43 +1,226 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+    <ion-content>
+
+      <ion-title>Home</ion-title>
+      <ion-grid fixed>
+        <ion-row class="statusrow">
+          <ion-col size="6">
+            <ion-card color ="dark">
+              <ion-card-header>
+                <ion-card-subtitle>Kolam</ion-card-subtitle>
+                <ion-card-title>10</ion-card-title>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+          <ion-col size="6">
+            <ion-card color ="dark">
+              <ion-card-header>
+                <ion-card-subtitle>Kolam Aktif</ion-card-subtitle>
+                <ion-card-title>8</ion-card-title>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+        <ion-row class="statusrow">
+          <ion-col size="6">
+            <ion-card color= "dark">
+              <ion-card-header>
+                <ion-card-subtitle>Ikan Hidup</ion-card-subtitle>
+                <ion-card-title>182 ekor</ion-card-title>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+          <ion-col size="6">
+            <ion-card color= "dark">
+              <ion-card-header>
+                <ion-card-subtitle>Ikan Mati</ion-card-subtitle>
+                <ion-card-title>89 ekor</ion-card-title>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+        <ion-row class="statusrow">
+          <ion-col size="6">
+            <ion-card color="dark">
+              <ion-card-header>
+                <ion-card-subtitle>Panen 2022</ion-card-subtitle>
+                <ion-card-title>109 Kg</ion-card-title>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+          <ion-col size="6">
+            <ion-card color="dark">
+              <ion-card-header>
+                <ion-card-subtitle>Total Pakan</ion-card-subtitle>
+                <ion-card-title>223 Kg</ion-card-title>
+              </ion-card-header>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-title>Total Berat Ikan</ion-title>
+      <swiper :slides-per-view="2" :loop="true">
+        <swiper-slide>
+          <ion-card color="dark" class="berat">
+            <img src="assets/img/lele.png" alt="lele" class="ikan">
+            <ion-card-header>
+              <ion-card-title><ion-text color="light"><h3 class="judul">Lele</h3></ion-text></ion-card-title>
+              <ion-text color="primary"><h4>200 Kg</h4></ion-text>
+            </ion-card-header>
+
+            <ion-card-content>
+            </ion-card-content>
+          </ion-card>
+        </swiper-slide>
+        <swiper-slide>
+          <ion-card color="dark" class="berat">
+            <img src="assets/img/nila.png" alt="nila" class="ikan">
+            <ion-card-header>
+              <ion-card-title><ion-text color="light"><h3 class="judul">Nila</h3></ion-text></ion-card-title>
+              <ion-text color="primary"><h4>200 Kg</h4></ion-text>
+            </ion-card-header>
+
+            <ion-card-content>
+            </ion-card-content>
+          </ion-card>
+        </swiper-slide>
+        <swiper-slide>
+          <ion-card color="dark" class="berat">
+            <img src="assets/img/lele.png" alt="lele" class="ikan">
+            <ion-card-header>
+              <ion-card-title><ion-text color="light"><h3 class="judul">Lele</h3></ion-text></ion-card-title>
+              <ion-text color="primary"><h4>200 Kg</h4></ion-text>
+            </ion-card-header>
+
+            <ion-card-content>
+            </ion-card-content>
+          </ion-card>
+        </swiper-slide>
+        <swiper-slide>
+          <ion-card color="dark" class="berat">
+            <img src="assets/img/nila.png" alt="nila" class="ikan">
+            <ion-card-header>
+              <ion-card-title><ion-text color="light"><h3 class="judul">Nila</h3></ion-text></ion-card-title>
+              <ion-text color="primary"><h4>200 Kg</h4></ion-text>
+            </ion-card-header>
+
+            <ion-card-content>
+            </ion-card-content>
+          </ion-card>
+        </swiper-slide>
+      </swiper>
+
+      <ion-title>Kualitas Air</ion-title>
+      <swiper :slides-per-view="2" :loop="true">
+        <swiper-slide>
+          <ion-card color="dark" class="kualitas">
+            <ion-card-header>
+              <ion-card-title><ion-text color="light" ><h1 class="judul">pH</h1></ion-text></ion-card-title>
+              <ion-row>
+                <ion-col size="6"></ion-col>
+                <ion-col size="6"></ion-col>
+              </ion-row>
+              <ion-row>
+                <ion-col size="6"></ion-col>
+                <ion-col size="6"></ion-col>
+              </ion-row>
+            </ion-card-header>
+
+            <ion-card-content>
+            </ion-card-content>
+          </ion-card>
+        </swiper-slide>
+        <swiper-slide>
+          <ion-card color="dark" class="kualitas">
+            <ion-card-header>
+              <ion-card-title><ion-text color="light"><h1 class="judul">DO</h1></ion-text></ion-card-title>
+              <ion-row>
+                <ion-col size="6"></ion-col>
+                <ion-col size="6"></ion-col>
+              </ion-row>
+              <ion-row>
+                <ion-col size="6"></ion-col>
+                <ion-col size="6"></ion-col>
+              </ion-row>
+            </ion-card-header>
+
+            <ion-card-content>
+            </ion-card-content>
+          </ion-card>
+        </swiper-slide>
+        <swiper-slide>
+          <ion-card color="dark" class="kualitas">
+            <ion-card-header>
+              <ion-card-title><ion-text color="light"><h1 class="judul">Suhu</h1></ion-text></ion-card-title>
+              <ion-row>
+                <ion-col size="6"></ion-col>
+                <ion-col size="6"></ion-col>
+              </ion-row>
+              <ion-row>
+                <ion-col size="6"></ion-col>
+                <ion-col size="6"></ion-col>
+              </ion-row>
+            </ion-card-header>
+
+            <ion-card-content>
+            </ion-card-content>
+          </ion-card>
+        </swiper-slide>
+      </swiper>
     </ion-content>
+
+    <ion-footer :translucent="true">
+      <ion-toolbar>
+        <ion-title></ion-title>
+      </ion-toolbar>
+    </ion-footer>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonPage, IonTitle, IonGrid, IonCol, IonCard, IonToolbar, IonFooter,IonText} from '@ionic/vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
 import { defineComponent } from 'vue';
+
+import 'swiper/css';
+import '@ionic/vue/css/ionic-swiper.css';
 
 export default defineComponent({
   name: 'HomePage',
   components: {
     IonContent,
-    IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonGrid,
+    IonCol,
+    IonCard,
+    IonToolbar,
+    IonFooter,
+    Swiper,
+    SwiperSlide,
+    IonText
+  },
+  setup() {
+    // Optional parameters to pass to the swiper instance. See https://swiperjs.com/swiper-api for valid options.
+    const slideOpts = {
+      initialSlide: 1,
+      speed: 400
+    };
+    return { slideOpts }
   }
+
 });
+
+
 </script>
 
 <style scoped>
+ion-title{
+  margin-top: 10px;
+}
+
 #container {
   text-align: center;
   
@@ -64,5 +247,23 @@ export default defineComponent({
 
 #container a {
   text-decoration: none;
+}
+
+ion-card{
+  border-radius: 10px;
+}
+img.ikan{
+  height: 150px;
+  width: 200px;
+}
+
+ion-text .judul{
+  text-align: left;
+  font-weight: bold;
+  margin-top: -10px;
+}
+
+ion-card.kualitas{
+  width: 400px;
 }
 </style>
---
+++ src/views/PondPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <ion-page>
+      <ion-content>
+        <h1>oke udah</h1>
+      </ion-content>
+  
+    </ion-page>
+  </template>
+  
+  <script lang="ts">
+  import { IonContent, IonPage} from '@ionic/vue';
+  import { defineComponent } from 'vue';
+  
+  
+  
+  export default defineComponent({
+    name: 'HomePage',
+    components: {
+      IonContent,
+      IonPage,
+    },
+    setup() {
+      // Optional parameters to pass to the swiper instance. See https://swiperjs.com/swiper-api for valid options.
+      const slideOpts = {
+        initialSlide: 1,
+        speed: 400
+      };
+      return { slideOpts }
+    }
+  
+  });
+  
+  
+  </script>
+  
+  <style scoped>
+
+  </style>
+  
